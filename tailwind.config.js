2e9c65e8337628a01adfd94683d7904e0265a3cb
jitendershiprocket <jitender.sharma@kartrocket.com>
2026-02-11 12:29:35 +0530

    workin gon plans

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,19 +28,19 @@ module.exports = {
        * standard text sizes to `px` values.
        */
       fontSize: {
-        xs: ['12px', { lineHeight: '16px' }],
-        sm: ['14px', { lineHeight: '20px' }],
-        base: ['16px', { lineHeight: '24px' }],
-        lg: ['18px', { lineHeight: '28px' }],
-        xl: ['20px', { lineHeight: '28px' }],
-        '2xl': ['24px', { lineHeight: '32px' }],
-        '3xl': ['30px', { lineHeight: '36px' }],
-        '4xl': ['36px', { lineHeight: '40px' }],
-        '5xl': ['48px', { lineHeight: '1' }],
-        '6xl': ['60px', { lineHeight: '1' }],
-        '7xl': ['72px', { lineHeight: '1' }],
-        '8xl': ['96px', { lineHeight: '1' }],
-        '9xl': ['128px', { lineHeight: '1' }],
+        xs: ['12px'],
+        sm: ['14px'],
+        base: ['16px'],
+        lg: ['18px'],
+        xl: ['20px'],
+        '2xl': ['24px'],
+        '3xl': ['30px'],
+        '4xl': ['36px'],
+        '5xl': ['48px'],
+        '6xl': ['60px'],
+        '7xl': ['72px'],
+        '8xl': ['96px'],
+        '9xl': ['128px'],
       },
       colors: {
         primary: {
